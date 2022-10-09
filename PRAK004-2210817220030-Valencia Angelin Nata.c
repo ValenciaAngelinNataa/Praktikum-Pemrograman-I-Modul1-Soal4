352b60e5528c087c23d60ebb47ca1e82fc68d8d6
ValenciaAngelinNataa <115389658+ValenciaAngelinNataa@users.noreply.github.com>
2022-10-09 00:53:18 -0700

    Add files via upload

---
+++ PRAK004-2210817220030-Valencia Angelin Nata.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf ("###########################\n");
+printf ("#                         #\n");
+printf ("#  Valencia Angelin Nata  #\n");
+printf ("#      2210817220030      #\n");
+printf ("#                         #\n");
+printf ("###########################\n");
+}
